eba527d9dbe1d6ed69108942f7e84c8847be5a08
Weslley Murdock <weslleyluiz@outlook.com.br>
2026-09-09 00:18:29 -0300

    chore(scripts): add mongodb helper script

--- scripts/postgres.ps1
+++ scripts/postgres.ps1
@@ -1,48 +1,54 @@
-# Nome do container e configurações
-$ContainerName = "memoana-postgres"
-$PortHost = "5432"
-$PortContainer = "5432"
-$DbName = "MemoAnaDb"
-$DbUser = "m3m04n4"
-$DbPassword = "M3m04N4@D!3"
-$VolumeName = "memoana-postgres-data"
-$ImageName = "postgres:17" 
+<#
+.SYNOPSIS
+    Script for instantiate an Postgres container through Docker.
+.DESCRIPTION
+    Creates an local volume for data persistency and runs the container with
+    settings loaded through script parameters or defaults if not supplied.
+#>
+
+param(
+    [string]$CN = "postgres",
+    [string]$PH = "5432",
+    [string]$DN = "postgres",
+    [string]$U = "postgres",
+    [string]$P = "p057_6R35",
+    [string]$VN = "postgres-data",
+    [string]$IN = "postgres:17") 
 
 $volumeExists = docker volume ls --q -f "name=^${VolumeName}$"
 if (-not $volumeExists) {
-    Write-Host "Criando volume de dados: $VolumeName..." -ForegroundColor Cyan
-    docker volume create $VolumeName | Out-Null
+    Write-Host "Creating data volume: $VN..." -ForegroundColor Cyan
+    docker volume create $VN | Out-Null
 }
 
 $containerExists = docker ps -a -q -f "name=^${ContainerName}$"
 
 if ($containerExists) {
-    $status = docker inspect --format='{{.State.Running}}' $ContainerName
+    $status = docker inspect --format='{{.State.Running}}' $CN
     
     if ($status -eq "true") {
-        Write-Host "O container '$ContainerName' já está rodando." -ForegroundColor Green
+        Write-Host "The container '$CN' is already running." -ForegroundColor Green
     } else {
-        Write-Host "Iniciando container '$ContainerName' existente..." -ForegroundColor Yellow
-        docker start $ContainerName | Out-Null
-        Write-Host "Container iniciado com sucesso!" -ForegroundColor Green
+        Write-Host "Starting existing container '$CN'..." -ForegroundColor Yellow
+        docker start $CN | Out-Null
+        Write-Host "Container successfully started!" -ForegroundColor Green
     }
 } else {
-    Write-Host "Criando e iniciando novo container '$ContainerName'..." -ForegroundColor Cyan
+    Write-Host "Creating and starting a new container '$CN'..." -ForegroundColor Cyan
     
     docker run -d `
-        --name $ContainerName `
-        -p "${PortHost}:${PortContainer}" `
-        -e POSTGRES_DB=$DbName `
-        -e POSTGRES_USER=$DbUser `
-        -e POSTGRES_PASSWORD=$DbPassword `
-        -v "${VolumeName}:/var/lib/postgresql/data" `
+        --name $CN `
+        -p "${PH}:5432" `
+        -e POSTGRES_DB=$DN `
+        -e POSTGRES_USER=$U `
+        -e POSTGRES_PASSWORD=$P `
+        -v "${VN}:/var/lib/postgresql/data" `
         --restart unless-stopped `
-        $ImageName | Out-Null
+        $IN | Out-Null
 
-    Write-Host "Container '$ContainerName' criado e rodando na porta $PortHost!" -ForegroundColor Green
+    Write-Host "Started container '$CN' at port $PH!" -ForegroundColor Green
 }
 
-# 3. Exibe o estado e logs rápidos
 Write-Host ""
-Write-Host "Status do Container:" -ForegroundColor Green
+Write-Host "Container Status:" -ForegroundColor Green
 docker ps -f "name=^${ContainerName}$"
